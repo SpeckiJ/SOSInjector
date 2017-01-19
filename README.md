d34f3c60d84040b1cc8635f54bd4f716a0de9a93
SpeckiJ <SpeckiJ@gmail.com>
2017-01-20 00:31:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Golang SOS Injector
+===
+
+This is a basic Program for injecting Air Quality Data automatically obtained from the "Umweltbundesamt" ([www.umweltbundesamt.de](www.umweltbundesamt.de)) into a SOS ([Sensor Observation Service](https://en.wikipedia.org/wiki/Sensor_Observation_Service)) Server.
+
+The Program is used in the Final Assignment in the course "SII - Spatial Information Infrastructures" in WWU Muenster ([link][1]).
+
+**Due to time restrictions the Program is programmed specific to this Use Case and not optimized to be portable in any way.**
+
+There is **NO** Documentation available as this program is not intended to be used outside of the Assignment.
+ 
+ [1]: https://uvlsf.uni-muenster.de/qisserver/rds?state=verpublish&status=init&vmfile=no&publishid=230590&moduleCall=webInfo&publishConfFile=webInfo&publishSubDir=veranstaltung " "
+
+
+
+##TODO:
+ - Error Handling 
+ - Log File Parsing
+ - CRON Job for automatic Insertion of Data
+ - Improve XML Templates (remove Dummy Variables)
+ - Documentation
---
+++ main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"gocsv"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"text/template"
+	"time"
+)
+
+type Sensor struct {
+	Lat         string `csv:"lat"`
+	Lon         string `csv:"lon"`
+	Stationcode string `csv:"stationCode"`
+	State       string `csv:"state"`
+	Title       string `csv:"title"`
+	RawDate     string `csv:"date"`
+	Date        string
+	RawVal      string `csv:"val"`
+	Val         string
+	Type2       string `csv:"type"`
+	Valtype     string `csv:"valtype"`
+	Unit        string
+}
+
+func main() {
+
+	// Currentdate
+	t := time.Now()
+	currentDate, _ := strconv.Atoi(t.Format("20060102"))
+
+	currentDate -= 1
+    
+	// Creating Sensor Objects
+	Data := getData(currentDate)
+
+	// Format Values inside Sensor struct
+	formatValues(Data)
+
+	// Filling Templates and posting Data to SOS
+	insertDataIntoSOS(Data, "InsertSensor.xml")
+	insertDataIntoSOS(Data, "InsertObservation.xml")
+}
+
+func getData(currentDate int) []*Sensor {
+
+	// Filling currentDate into URL
+	url := fmt.Sprintf("https://www.umweltbundesamt.de/luftdaten/stations/locations?pollutant=PM1&data_type=1TMW&date=%v", currentDate)
+
+    fmt.Println(url)
+    
+	// Build HTTP GET Request
+	req, err := http.NewRequest("GET", url, nil)
+	// Fehlerbehandlung
+	if err != nil {
+		log.Fatal("Error on building Request to Data Server", err)
+		return nil
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	// Fehlerbehandlung
+	if err != nil {
+		log.Fatal("Error on Response from Data Server", err)
+		return nil
+	}
+	// No idea what this is for
+	defer resp.Body.Close()
+
+	// Reading Response
+	responseBodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading Data", err)
+		return nil
+	}
+
+	// Parsing Response to proper CSV Format
+	responseBodyCSV := string(responseBodyBytes)
+	responseBodyCSV = strings.Replace(responseBodyCSV, ",", " ", -1)
+	responseBodyCSV = strings.Replace(responseBodyCSV, "	", "\",\"", -1)
+	responseBodyCSV = strings.Replace(responseBodyCSV, "\n", "\"\n\"", -1)
+	responseBodyCSV = strings.Replace(responseBodyCSV, "&uuml;", "ü", -1)
+	responseBodyCSV = strings.Replace(responseBodyCSV, "&auml;", "ä", -1)
+	responseBodyCSV = strings.Replace(responseBodyCSV, "&ouml;", "ö", -1)
+	responseBodyCSV = strings.Replace(responseBodyCSV, "&szlig;", "ß", -1)
+	responseBodyCSV = "\"" + responseBodyCSV + "\""
+    
+	// Slice for Storing Sensors
+	sensors := []*Sensor{}
+
+	// CSV to struct
+	if err := gocsv.UnmarshalString(responseBodyCSV, &sensors); err != nil {
+		log.Fatal("Error Converting CSV to struct")
+	}
+
+	return sensors
+}
+func insertDataIntoSOS(sensors []*Sensor, filledtemplate string) bool {
+
+	// Loading Template
+	var template = template.Must(template.ParseFiles("./template/" + filledtemplate))
+
+	url := "http://127.0.0.1:9001/52n-sos-webapp/service"
+
+	for _, sensor := range sensors {
+		// Buffer Filled Template
+		var buffer bytes.Buffer
+		// Fill Template with Data
+		if err := template.Execute(&buffer, sensor); err != nil {
+			log.Fatal("Error on building Template for Request to Data Server", err)
+			return false
+		}
+		postingDataToSOS(url, buffer)
+	}
+	return true
+}
+
+func postingDataToSOS(url string, body bytes.Buffer) bool {
+
+	// Creating POST Request
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body.Bytes()))
+	if err != nil {
+		log.Fatal("Error on creating POST Request")
+		return false
+	}
+
+	// Setting appropiate Content Header
+	req.Header.Set("Content-Type", "application/xml")
+
+	// Posting POST Request to Server
+	client := &http.Client{}
+	_ , err = client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+	return true
+}
+
+func formatValues(sensors []*Sensor) {
+	for _, sensor := range sensors {
+		date := strings.Split(sensor.RawDate, ".")
+		sensor.Date = date[2] + "-" + date[1] + "-" + date[0]
+
+		value := strings.Split(sensor.RawVal, " ")
+		sensor.Val = value[0]
+		sensor.Unit = value[1]
+	}
+}
